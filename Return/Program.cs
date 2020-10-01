710ab0aff3c6de8fcadbf574ea45789b131ebb6f
Mikael <mikael.bergstrom@ga.ntig.se>
2020-10-01 10:18:04 +0200

    Initial commit

---
+++ Return/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Return
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int hp = 100;
+
+      string answer = AskQuestion();
+
+      Console.WriteLine("Du skrev " + answer);
+
+      Console.ReadLine();
+    }
+
+    static string AskQuestion()
+    {
+      Console.WriteLine("Skriv A, B eller C");
+      string a = Console.ReadLine();
+''
+      return a;
+    }
+
+    // static void Hello()
+    // {
+    //   hp = 50;
+
+    //   int strength = 100;
+
+    //   Console.WriteLine("hej");
+    // }
+
+  }
+}
